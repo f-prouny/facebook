d670654d11f593b07caf1f1edbd06d9586906a6f
f-prouny <f.prouny76@gmail.com>
2022-10-25 14:04:09 +0200

    installation d'un user

---
+++ facebook/src/Controller/RegistrationController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use App\Security\AuthAuthenticator;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class RegistrationController extends AbstractController
+{
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AuthAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->get('plainPassword')->getData() ===
+             $form->get('confirmPassword')->getData()
+             ) {
+                // comment obtenir l'url du dossier public ?
+                $dirUpload = str_replace("\\", "/", $this->getParameter('upload_directory'))."/";
+            // utiliser la fonction moveUpLoadFile de php
+                move_uploaded_file($_FILES['registration_form']['tmp_name']['avatar'],
+                $dirUpload.$_FILES['registration_form']['name']['avatar']);
+                // redimentionner mon image + changer 
+                dd($dirUpload);
+                $user->setRoles(['ROLE_USER']
+                );
+                $user->setPassword(
+                    $userPasswordHasher->hashPassword(
+                        $user,
+                        $form->get('plainPassword')->getData()
+                    )
+                );
+
+                $entityManager->persist($user);
+                $entityManager->flush();
+                // do anything else you need here, like send an email
+
+                return $userAuthenticator->authenticateUser(
+                    $user,
+                    $authenticator,
+                    $request
+                );
+            } 
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
+            
+        ]);
+    }
+}
